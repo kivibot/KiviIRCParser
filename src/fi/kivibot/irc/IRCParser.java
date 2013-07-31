090993bc90396c2fa460329ddf248b4bd7d820be
kivibot <kivi@kivibot.fi>
2013-08-01 02:49:43 +0300

    Listeners

--- src/fi/kivibot/irc/IRCParser.java
+++ src/fi/kivibot/irc/IRCParser.java
@@ -42,84 +42,84 @@ public class IRCParser {
             ol.onIRCEvent(pe);
         } else {
             String command = parts[1];
-            
+
             if (isNumeric(command)) {
-                String[] sr = new String[parts.length-2];
+                String[] sr = new String[parts.length - 2];
                 System.arraycopy(parts, 2, sr, 0, sr.length);
                 CommandResponseEvent cre = new CommandResponseEvent(server, Integer.parseInt(parts[1]), sr);
                 ol.onCommandResponseEvent(cre);
                 ol.onIRCEvent(cre);
                 return;
             }
-            
-            String[] usr = parts[0].contains("!")?parts[0].split("!"):new String[]{parts[0],parts[0]};
+
+            String[] usr = parts[0].contains("!") ? parts[0].split("!") : new String[]{parts[0], parts[0]};
             switch (command.toUpperCase()) {
                 case "NICK":
-                    NickEvent ne = new NickEvent(server,usr[0],usr[1],parts[2]);
+                    NickEvent ne = new NickEvent(server, usr[0], usr[1], parts[2]);
                     ol.onNickEvent(ne);
                     ol.onIRCEvent(ne);
                     break;
                 case "QUIT":
-                    QuitEvent qe = new QuitEvent(server,usr[0],usr[1],parts[2]);
+                    QuitEvent qe = new QuitEvent(server, usr[0], usr[1], parts[2]);
                     ol.onQuitEvent(qe);
                     ol.onIRCEvent(qe);
                     break;
                 case "JOIN":
-                    JoinEvent je = new JoinEvent(server,usr[0],usr[1],parts[2]);
+                    JoinEvent je = new JoinEvent(server, usr[0], usr[1], parts[2]);
                     ol.onJoinEvent(je);
                     ol.onIRCEvent(je);
                     break;
                 case "PART":
-                    PartEvent pe = new PartEvent(server,usr[0],usr[1],parts[2],parts[3]);
+                    PartEvent pe = new PartEvent(server, usr[0], usr[1], parts[2], parts[3]);
                     ol.onPartEvent(pe);
                     ol.onIRCEvent(pe);
                     break;
                 case "MODE":
-                    ModeEvent me = new ModeEvent(server,usr[0],usr[1],parts[2],(parts.length>4?parts[4]:""),parts[3]);
+                    ModeEvent me = new ModeEvent(server, usr[0], usr[1], parts[2], (parts.length > 4 ? parts[4] : ""), parts[3]);
                     ol.onModeEvent(me);
                     ol.onIRCEvent(me);
                     break;
                 case "TOPIC":
-                    TopicEvent te = new TopicEvent(server,usr[0],usr[1],parts[2],parts[3]);
+                    TopicEvent te = new TopicEvent(server, usr[0], usr[1], parts[2], parts[3]);
                     ol.onTopicEvent(te);
                     ol.onIRCEvent(te);
                     break;
                 case "INVITE":
-                    InviteEvent ie = new InviteEvent(server,usr[0],usr[1],parts[3],parts[2]);
+                    InviteEvent ie = new InviteEvent(server, usr[0], usr[1], parts[3], parts[2]);
                     ol.onInviteEvent(ie);
                     ol.onIRCEvent(ie);
                     break;
                 case "KICK":
-                    KickEvent ke = new KickEvent(server,usr[0],usr[1],parts[2],parts[3],parts.length==5?parts[4]:"");
+                    KickEvent ke = new KickEvent(server, usr[0], usr[1], parts[2], parts[3], parts.length == 5 ? parts[4] : "");
                     ol.onKickEvent(ke);
                     ol.onIRCEvent(ke);
                     break;
                 case "PRIVMSG":
-                    if(parts[3].startsWith("\u0001")){
+                    if (parts[3].startsWith("\u0001")) {
                         //ctcp
                         String[] pt = parts[3].replace("\u0001", "").split(" ");
                         String asd = "";
-                        for(int i=1; i<pt.length-1; i++){
+                        for (int i = 1; i < pt.length - 1; i++) {
                             asd += pt[i] + " ";
                         }
-                        asd += pt[pt.length-1];
-                        CtcpEvent cp = new CtcpEvent(server,usr[0],usr[1],parts[2],pt[0],asd);
+                        asd += pt[pt.length - 1];
+                        CtcpEvent cp = new CtcpEvent(server, usr[0], usr[1], parts[2], pt[0], asd);
                         ol.onCtcpEvent(cp);
                         ol.onIRCEvent(cp);
-                    }else{
+                    } else {
                         //normal
-                        PrivmsgEvent pme = new PrivmsgEvent(server,usr[0],usr[1],parts[2],parts[3]);
+                        PrivmsgEvent pme = new PrivmsgEvent(server, usr[0], usr[1], parts[2], parts[3]);
                         ol.onPrivmsgEvent(pme);
                         ol.onIRCEvent(pme);
                     }
                     break;
                 case "NOTICE":
-                    NoticeEvent noe = new NoticeEvent(server,usr[0],usr[1],parts[2],parts[3]);
+                    NoticeEvent noe = new NoticeEvent(server, usr[0], usr[1], parts[2], parts[3]);
                     ol.onNoticeEvent(noe);
                     ol.onIRCEvent(noe);
                     break;
                 case "WALLOPS":
-                    WallopsEvent we = new WallopsEvent(server,parts[2]);
+                    WallopsEvent we = new WallopsEvent(server, parts[2]);
                     ol.onWallopsEvent(we);
                     ol.onIRCEvent(we);
                     break;
@@ -149,11 +149,11 @@ public class IRCParser {
                 st = false;
                 continue;
             }
-            if(!st && c == ':'){
+            if (!st && c == ':') {
                 msg = true;
                 continue;
             }
-            if(st && c == ':'){
+            if (st && c == ':') {
                 continue;
             }
             s += c;
